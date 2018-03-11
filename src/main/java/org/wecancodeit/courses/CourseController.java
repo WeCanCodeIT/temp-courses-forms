8fbf8177e61f38de0553be8176a9b3af58701ab0
AlanKostrick <alan@wecancodeit.org>
2018-03-11 06:42:13 -0400

    solution

--- src/main/java/org/wecancodeit/courses/CourseController.java
+++ src/main/java/org/wecancodeit/courses/CourseController.java
@@ -13,6 +13,9 @@ public class CourseController {
 	@Resource
 	CourseRepository courseRepo;
 
+	@Resource
+	InstructorRepository instructorRepo;
+
 	@RequestMapping("/show-courses")
 	public String findAllCourses(Model model) {
 		model.addAttribute("courses", courseRepo.findAll());
@@ -23,6 +26,39 @@ public class CourseController {
 	public String findOneCourse(@RequestParam(value = "id") Long id, Model model) {
 		model.addAttribute("courses", courseRepo.findOne(id));
 		return "course";
+	}
+
+	@RequestMapping("/add-course")
+	public String addCourse(String name, String description, String instructorName) {
+
+		Instructor instructor = instructorRepo.findByName(instructorName);
+
+		Course newCourse = courseRepo.findByName(name);
+		if (newCourse == null) {
+			newCourse = new Course(name, description, instructor);
+			courseRepo.save(newCourse);
+		}
+
+		return "redirect:/show-courses";
+	}
+
+	@RequestMapping("remove-course")
+	public String removeCourse(String name) {
+
+		Course courseToRemove = courseRepo.findByName(name);
+		if (courseRepo.findByName(name) != null) {
+			courseRepo.delete(courseToRemove);
+			return "redirect:/show-courses";
+		}
+
+		return "redirect:/show-courses";
+	}
+
+	@RequestMapping("find-by-instructor")
+	public String findCoursesByInstructor(String instructorName, Model model) {
+		Instructor instructor = instructorRepo.findByName(instructorName);
+		model.addAttribute("courses", courseRepo.findByInstructor(instructor));
+		return "/instructor";
 	}
 
 }
