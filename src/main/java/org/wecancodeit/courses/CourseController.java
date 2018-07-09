169fb09a6eb12393744fa779ec387379d5a64c20
Columbus Instructor <instructor@wecancodeit.org>
2018-07-09 15:33:10 -0400

    Change a ton of stuff

--- src/main/java/org/wecancodeit/courses/CourseController.java
+++ src/main/java/org/wecancodeit/courses/CourseController.java
@@ -4,8 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CourseController {
@@ -13,15 +14,26 @@ public class CourseController {
 	@Resource
 	CourseRepository courseRepo;
 
-	@RequestMapping("/show-courses")
-	public String findAllCourses(Model model) {
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/courses";
+	}
+
+	@RequestMapping(value = "/courses", method = RequestMethod.GET)
+	public String getCourses(Model model) {
 		model.addAttribute("courses", courseRepo.findAll());
 		return "courses";
 	}
 
-	@RequestMapping("/course")
-	public String findOneCourse(@RequestParam(value = "id") Long id, Model model) {
-		model.addAttribute("courses", courseRepo.findOne(id));
+	@RequestMapping(value = "/courses", method = RequestMethod.POST)
+	public String addCourse(String courseName, String courseDescription, String courseInstructor) {
+		courseRepo.save(new Course(courseName, courseDescription, courseInstructor));
+		return "redirect:/courses";
+	}
+
+	@RequestMapping("/courses/{id}")
+	public String course(@PathVariable(name = "id") Long id, Model model) {
+		model.addAttribute("course", courseRepo.findOne(id));
 		return "course";
 	}
 
--- src/main/java/org/wecancodeit/courses/CoursePopulator.java
+++ src/main/java/org/wecancodeit/courses/CoursePopulator.java
@@ -3,9 +3,9 @@ package org.wecancodeit.courses;
 import javax.annotation.Resource;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CoursePopulator implements CommandLineRunner {
 
 	@Resource
@@ -15,7 +15,7 @@ public class CoursePopulator implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		Course java = new Course("Java", "Java Description", "Alan");
-		Course javascript = new Course("Javascript", "Javascript Description", "Donny");
+		Course javascript = new Course("JavaScript", "JavaScript Description", "Donny");
 		Course spring = new Course("Spring", "Spring Description", "Brian");
 
 		courseRepo.save(java);
