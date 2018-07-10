c537e4be10c57856ecb0e2ba5ab5554010ea626f
Columbus Instructor <instructor@wecancodeit.org>
2018-07-10 10:40:49 -0400

    Finish example

--- src/main/java/org/wecancodeit/courses/CourseController.java
+++ src/main/java/org/wecancodeit/courses/CourseController.java
@@ -14,12 +14,15 @@ public class CourseController {
 	@Resource
 	CourseRepository courseRepo;
 
+	@Resource
+	InstructorRepository instructorRepo;
+
 	@RequestMapping("/")
 	public String home() {
 		return "redirect:/courses";
 	}
 
-	@RequestMapping(value = "/courses", method = RequestMethod.GET)
+	@RequestMapping("/courses")
 	public String getCourses(Model model) {
 		model.addAttribute("courses", courseRepo.findAll());
 		return "courses";
@@ -27,7 +30,15 @@ public class CourseController {
 
 	@RequestMapping(value = "/courses", method = RequestMethod.POST)
 	public String addCourse(String courseName, String courseDescription, String courseInstructor) {
-		courseRepo.save(new Course(courseName, courseDescription, courseInstructor));
+		if (courseRepo.findByName(courseName) == null) {
+			Instructor instructor;
+			if (instructorRepo.findByName(courseInstructor) == null) {
+				instructor = instructorRepo.save(new Instructor(courseInstructor));
+			} else {
+				instructor = instructorRepo.findByName(courseInstructor);
+			}
+			courseRepo.save(new Course(courseName, courseDescription, instructor));
+		}
 		return "redirect:/courses";
 	}
 
--- src/main/java/org/wecancodeit/courses/CoursePopulator.java
+++ src/main/java/org/wecancodeit/courses/CoursePopulator.java
@@ -11,12 +11,19 @@ public class CoursePopulator implements CommandLineRunner {
 	@Resource
 	private CourseRepository courseRepo;
 
+	@Resource
+	private InstructorRepository instructorRepo;
+
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Instructor alan = instructorRepo.save(new Instructor("Alan"));
+		Instructor donny = instructorRepo.save(new Instructor("Donny"));
+		Instructor brian = instructorRepo.save(new Instructor("Brian"));
 
-		Course java = new Course("Java", "Java Description", "Alan");
-		Course javascript = new Course("JavaScript", "JavaScript Description", "Donny");
-		Course spring = new Course("Spring", "Spring Description", "Brian");
+		Course java = new Course("Java", "Java Description", alan);
+		Course javascript = new Course("JavaScript", "JavaScript Description", donny);
+		Course spring = new Course("Spring", "Spring Description", brian);
 
 		courseRepo.save(java);
 		courseRepo.save(javascript);
